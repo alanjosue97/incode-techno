67a868ea59a45679d57501fbc26c4ec72e1de3f5
alanjosue97 <33445418+alanjosue97@users.noreply.github.com>
2021-05-05 15:22:39 -0500

    home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import incode from './assets/incode.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import BiometricFace from "./componets/biometric.js";
 
-function App() {
+function App(){
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/componets/biometric">
+            <BiometricFace/>
+          </Route>
+          <Route path="/home">
+            <Home/>
+          </Route>
+        </Switch>
+       </Router>
+       </div>   
+  
+    )
+
+}
+
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload prueba de merge.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={incode} className="Incode-logo" alt="logo" />
+        <Link to="./componets/biometric"><button className="find-id">Find Identities</button></Link> 
+         <Link><button className='tabla-id'>Identites</button></Link>
+        <PurebaTxt/>
       </header>
+      
     </div>
+    
+   
   );
 }
 
+function PurebaTxt(){
+  return <h1>prueba texto</h1>
+}
 export default App;
---
+++ src/componets/biometric.js
@@ -0,0 +1,12 @@
+import "../App.css"
+
+function BiometricFace(){
+    return (
+        <div>
+            <h1>Finde Identities</h1>
+        </div>
+    )
+      
+}
+
+export default BiometricFace;
