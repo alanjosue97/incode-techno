8da8fd34044b2619c23151f4a7b7e9634d8d53ef
alanjosue97 <33445418+alanjosue97@users.noreply.github.com>
2021-05-05 13:40:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload prueba de merge.
         </p>
         <a
           className="App-link"
