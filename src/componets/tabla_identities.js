d46140c41c3d14c692035a2113a2568def9bff84
alanjosue97 <33445418+alanjosue97@users.noreply.github.com>
2021-05-07 01:08:14 -0500

    styles identities DONE!

--- src/componets/tabla_identities.js
+++ src/componets/tabla_identities.js
@@ -6,24 +6,35 @@ import Home from "../App.js";
 function Identities(){
     return(
         <div>
-        <TxtIdentities/>
+         <TxtIdentities/>
+        <Date/>
         <TablaIdentities/>
-        <Router>
+        <Pagination/>
+         <Router>
             <Switch>
                 <Route path="/home">
                     <Home/>
                 </Route>
             </Switch>
         </Router>
+        
         </div>
     );
 }
 function TxtIdentities(){
     return(
-        <div>
-            <h1 className="identities-txt">Identities <Link to="../home"><button className="btn">button</button></Link></h1>
+        <div className="txt-identi">
+            <h1 className="identities-txt is-desktop">Identities</h1>
+            <Link to="../home" className="button is-default is-light btn">Log out</Link>
         </div> 
         
+    );
+}
+function Date(){
+    return(
+        <div className="datefilter">
+            <h1>DATE</h1>
+        </div>
     );
 }
 
@@ -32,12 +43,12 @@ const getDataFromApi = () => new Promise((resolve => {
     setTimeout(()=> {
         resolve(
             [
-                {id: 1, name: 'pedro', phone: '214123', date: 'today', hour: '1pm',},
-                {id: 2, name: 'juan', phone: '324325', date: 'monday', hour: '5pm',},
-                {id: 3, name: 'alonso', phone: '435432', date: 'saturday', hour: '7pm',},
-                {id: 4, name: 'andre', phone: '346345', date: 'thursday', hour: '2pm',},
-                {id: 5, name: 'miguel', phone: '345345', date: 'Tuesday', hour: '9pm',},
-                {id: 6, name: 'romel', phone: '567465', date: 'sunday', hour: '8pm',}
+                {id: 1, name: 'Pedro', phone: '214123', date: 'today', hour: '1pm',},
+                {id: 2, name: 'Juan', phone: '324325', date: 'monday', hour: '5pm',},
+                {id: 3, name: 'Alonso', phone: '435432', date: 'saturday', hour: '7pm',},
+                {id: 4, name: 'Andre', phone: '346345', date: 'thursday', hour: '2pm',},
+                {id: 5, name: 'Miguel', phone: '345345', date: 'Tuesday', hour: '9pm',},
+                {id: 6, name: 'Romel', phone: '567465', date: 'sunday', hour: '8pm',}
             ]
         )
     }, 300)
@@ -68,35 +79,51 @@ function TablaIdentities(){
         }))
     }
     return(
-        <div>
-            <table>
-                <thead>
+        <div className="columns is-desktop is-centered tb">
+            <table className="table is-desktop is-centered ">
+                <thead className="head ">
                     <tr key={users.id}>
-                    <td className="check"><input type="checkbox" checked={users.allSelected} onChange={() => checkedHandler(users.id)} ></input></td>
-                    <td className="name "><element title="Name">Name</element></td>
-                    <td className="phone"><element title="Phone number">Phone Number</element></td>
-                    <td className="date"><element title="Date">Date</element></td>
-                    <td className="hour"><element title="Hour">Hour</element></td>
+                    <th className="check"><input type="checkbox" checked={users.allSelected} onChange={() => checkedHandler(users.id)} ></input></th>
+                    <th className="name has-text-grey-lighter" title="Name">Name</th>
+                    <th className="phone has-text-grey-lighter"  title="Phone number" >Phone Number</th>
+                    <th className="date has-text-grey-lighter" title="Date">Date</th>
+                    <th className="hour has-text-grey-lighter" title="Hour">Hour</th>
                     </tr>
                 </thead>
-            </table>
+            
             <tbody>
                {users.map(user => {
                    return(
                        <tr key={user.id}>
-                           <td><input type="checkbox" checked={user.isSelected} onChange={()=> checkedHandler(user.id)}></input></td>
-                           <td>{user.name}</td>
-                           <td>{user.phone}</td>
-                           <td>{user.date}</td>
-                           <td>{user.hour}</td>
+                           <th className="py-4"><input type="checkbox" checked={user.isSelected} onChange={()=> checkedHandler(user.id)}></input></th>
+                           <th className="py-4">{user.name}</th>
+                           <th className="py-4">{user.phone}</th>
+                           <th className="py-4 has-text-grey-light" >{user.date}</th>
+                           <th className="py-4 has-text-grey-light">{user.hour}</th>
                        </tr>
                    )
                })} 
             </tbody>
+        </table>
+    
         </div>
+        
+        
     );
 }
 
+function Pagination(){
+    return(
+        <div className="pag is-desktop is-centered">
+           <ul className ="pagination">
+           <li className="previus"><a href="#">❮</a></li>
+           <li className="num"><a href="#">1</a></li>
+           <li className="next"><a href="#">❯</a></li>
+        </ul>
+
+        </div>
+    );
+}
 
 
 
